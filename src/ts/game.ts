0de828cf2d041ead0e8499514e83b6bd183abfb4
bek77g <d.zheenkul@gmail.com>
2023-07-27 22:04:51 +0600

    init: completed game

--- src/ts/game.ts
+++ src/ts/game.ts
@@ -4,6 +4,7 @@ const gameDiv: HTMLDivElement = document.getElementById(
 const logoH1: HTMLHeadingElement = document.getElementById(
   'logo'
 ) as HTMLHeadingElement;
+let cells: NodeListOf<Element> | null = null;
 
 const winningCombinations: number[][] = [
   [0, 1, 2],
@@ -24,8 +25,10 @@ const createPlaceHoldersHTML = (): HTMLDivElement => {
   for (let i = 0; i < 9; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
+    cell.addEventListener('click', handleCellClick);
     board.appendChild(cell);
   }
+  cells = board.querySelectorAll('.cell');
   return board;
 };
 
@@ -40,3 +43,71 @@ export const startGame = (): void => {
     }
   });
 };
+
+function handleCellClick(event: Event) {
+  const cell = event.target as HTMLElement;
+
+  if (cell.textContent === '') {
+    cell.textContent = currentPlayer;
+    cell.classList.add(`cell-${currentPlayer.toLowerCase()}`);
+
+    if (checkWin(currentPlayer)) {
+      setTimeout(() => {
+        alert(currentPlayer + ' wins');
+        resetBoard();
+      }, 100);
+    } else if (isBoardFull()) {
+      setTimeout(() => {
+        alert("It's a tie!");
+        resetBoard();
+      }, 100);
+    } else {
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      if (currentPlayer === 'O') {
+        makeAIMove();
+      }
+    }
+  }
+}
+
+function checkWin(player: 'X' | 'O'): boolean {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => cells[index].textContent === player);
+  });
+}
+
+function isBoardFull(): boolean {
+  return [...cells].every((cell) => cell.textContent !== '');
+}
+
+function resetBoard() {
+  cells.forEach((cell) => {
+    cell.textContent = '';
+    cell.classList.remove('o', 'x');
+  });
+  currentPlayer = 'X';
+}
+
+function makeAIMove() {
+  const emptyCells = [...cells].filter((cell) => cell.textContent === '');
+  const randomIndex = Math.floor(Math.random() * emptyCells.length);
+  const cell = emptyCells[randomIndex] as HTMLElement;
+
+  setTimeout(() => {
+    cell.textContent = currentPlayer;
+    cell.classList.add(`cell-${currentPlayer.toLowerCase()}`);
+    if (checkWin(currentPlayer)) {
+      setTimeout(() => {
+        alert(currentPlayer + ' wins!');
+        resetBoard();
+      }, 100);
+    } else if (isBoardFull()) {
+      setTimeout(() => {
+        alert("It's a tie!");
+        resetBoard();
+      }, 100);
+    } else {
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+  }, 500);
+}
